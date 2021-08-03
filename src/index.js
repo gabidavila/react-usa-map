b81569e00bd1ccaedd9c61e5b6907395bef30973
nate pappenhagen <natepappenhagen@gmail.com>
2021-08-03 11:04:57 -0600

    make width and height accept `string` or `number`

--- src/index.js
+++ src/index.js
@@ -52,8 +52,8 @@ class USAMap extends React.Component {
 
 USAMap.propTypes = {
   onClick: PropTypes.func.isRequired,
-  width: PropTypes.number,
-  height: PropTypes.number,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   title: PropTypes.string,
   defaultFill: PropTypes.string,
   customize: PropTypes.object
